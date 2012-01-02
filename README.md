00fef27729c55039a5e2dab3b089fa61a3d1177d
Jonathan Mahoney <jonathan@mahoney.eu>
2012-01-02 17:03:47 +0000

    Added 'standalone file' functionality

---
+++ README.md
@@ -0,0 +1,27 @@
+###Overview
+
+Pattern-Primer-on-Node is a node.js port of Jeremy Keith's [Pattern Primer](https://github.com/adactio/Pattern-Primer), which is written in PHP.
+
+>Create little snippets of markup and save them to the "patterns folder." The pattern primer will generate a list of all the patterns in that folder. You will see the pattern rendered as HTML. You will also get the source displayed in a textarea.
+
+###Installation
+
+You'll need node.js installed (obviously). In addition, you'll need connect:
+
+    npm install connect
+		
+###Simple Usage
+
+Place all your HTML extracts into the `<root>/public/patterns` folder. Navigate to the root directory of Pattern-Primer-on-Node and run: 
+	
+    node pattern-primer.js
+	
+You can then navigate to http://localhost:8080/ to see the output.
+
+###Headless Operation
+
+If you want to generate a 'standalone' version of the primer output, then you can also run the program with the `tofile` switch as follows:
+
+   node pattern-primer.js --tofile
+   
+This will place a standalone html file, and a copy of the 'global.css' file located in `<root>/public` and will place them in `<root>/docs`. You should just be able to navigate to that folder and open the index.html file to see the same output.
--- pattern-primer.js
+++ pattern-primer.js
@@ -1,7 +1,14 @@
-var util = require('util'),
+var settings = {
+		webserverport : '8080',
+		wwwroot: 'public',
+		pattern_path: 'public/patterns',
+		sourcehtmlfile: 'source.html',
+		tofile_outputpath: 'docs'
+	},
+	util = require('util'),
 	connect = require('connect'),
 	server = connect.createServer(
-		connect.static(__dirname + '/public'),
+		connect.static(__dirname + '/' + settings.wwwroot),
 		function (req, resp) {
 			if (req.url !== '/') {
 				resp.writeHead(404, {
@@ -12,77 +19,107 @@ var util = require('util'),
 				return;
 			}
 			
-			var fs = require('fs'),
-				patternFolder = 'public/patterns',
-				simpleEscaper = function (text) {
-				   return text.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/"/g,'&quot;').replace(/'/g,'&#039;');
-				},
-				beginProcess = function() {
-					fs.readdir(patternFolder, function (err, contents) {
-						if (err !== null && err.code === 'ENOENT') {
-							util.puts('Cannot find patterns folder:', patternFolder);
-							return;
-						}
+			primer(resp);
+		}
+	),
+	primer = function(serverResponse, tofile, tofileCallback) {
+		tofile || false;
 		
-						var files = [],
-							i,
-							l;
+		var fs = require('fs'),
+			patternFolder = './' + settings.pattern_path,
+			simpleEscaper = function (text) {
+			   return text.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/"/g,'&quot;').replace(/'/g,'&#039;');
+			},
+			beginProcess = function() {
+				fs.readdir(patternFolder, function (err, contents) {
+					if (err !== null && err.code === 'ENOENT') {
+						util.puts('Cannot find patterns folder:', patternFolder);
+						return;
+					}
 		
-						for (i = 0, l = contents.length; i < l; i += 1) {
-							if (contents[i].substr(-5) === '.html') {
-								files.push(contents[i]);
-							}
+					var files = [],
+						i,
+						l;
+		
+					for (i = 0, l = contents.length; i < l; i += 1) {
+						if (contents[i].substr(-5) === '.html') {
+							files.push(contents[i]);
 						}
+					}
 			
-						handleFiles(files);
-					});
-				},
-				handleFiles = function (files) {
+					handleFiles(files);
+				});
+			},
+			handleFiles = function (files) {
+				var i,
+					l,
+					file,
+					patterns = [];
+			
+				// This was asyncronous, but we need the file names, which we can't get from the callback of 'readFile'
+				for (i = 0, l = files.length; i < l; i += 1) {
+					file = {
+						filename : files[i]
+					};
+						
+					file.content = fs.readFileSync(patternFolder + '/' + file.filename, 'utf-8');
+					patterns.push(file);
+				}
+					
+				outputPatterns(patterns);
+			},
+			outputPatterns = function (patterns) {
+				fs.readFile(settings.sourcehtmlfile, 'utf-8', function(err, content) {
+					if (err !== null) {
+						util.puts('There was an error when trying to read file:', 'output.html');
+						return;
+					}
+			
 					var i,
 						l,
-						file,
-						patterns = [];
+						file;
 			
-					// This was asyncronous, but we need the file names, which we can't get from the callback of 'readFile'
-					for (i = 0, l = files.length; i < l; i += 1) {
-						file = {
-							filename : files[i]
-						};
-						
-						file.content = fs.readFileSync(patternFolder + '/' + file.filename, 'utf-8');
-						patterns.push(file);
+					for (i = 0, l = patterns.length; i < l; i += 1) {
+						file = patterns[i];
+						content += '<div class="pattern"><div class="display">';
+						content += file.content;
+					    content += '</div><div class="source"><textarea rows="6" cols="30">';
+					    content += simpleEscaper(file.content);
+					    content += '</textarea>';
+						if (!tofile) {
+							content += '<p><a href="patterns/' + file.filename + '">' + file.filename + '</a></p>';
+						}
+						content += '</div></div>';
 					}
-					
-					outputPatterns(patterns);
-				},
-				outputPatterns = function (patterns) {
-					fs.readFile('output.html', 'utf-8', function(err, content) {
-						if (err !== null) {
-							util.puts('There was an error when trying to read file:', 'output.html');
-							return;
-						}
 			
-						var i,
-							l,
-							file;
+					content +='</body></html>';
 			
-						for (i = 0, l = patterns.length; i < l; i += 1) {
-							file = patterns[i];
-							content += '<div class="pattern"><div class="display">';
-							content += file.content;
-						    content += '</div><div class="source"><textarea rows="6" cols="30">';
-						    content += simpleEscaper(file.content);
-						    content += '</textarea><p><a href="patterns/' + file.filename + '">' + file.filename + '</a></p></div></div>';
-						}
+					if (tofile) { 
+						tofileCallback(content);
+					} else {
+						serverResponse.end(content);
+					}
+				});
+			};
 			
-						content +='</body></html>';
-			
-						resp.end(content);
-					});
-				};
-			
-			beginProcess();
-		}
-	).listen(8080);
+		beginProcess();
+	};
 
-util.puts('You can now visit http://localhost:8080/ to see your patterns.');
+if (process.argv[2] === '--tofile') {
+	
+	primer(null, true, function (content) {
+		var fs = require('fs');
+		fs.writeFile('./' + settings.tofile_outputpath + '/index.html', content, 'utf-8', function() {
+			util.pump(fs.createReadStream('./' + settings.wwwroot + '/global.css'), 
+				fs.createWriteStream('./' + settings.tofile_outputpath + '/global.css'));
+			util.puts('Stand-alone output can now be found in "' + settings.tofile_outputpath + '/"');
+		});
+	});
+		
+} else {
+	
+	server.listen(settings.webserverport);
+	util.puts('You can now visit http://localhost:' + settings.webserverport + '/ to see your patterns.');
+	util.puts('To kill this server, just press Ctrl + C');
+		
+}
